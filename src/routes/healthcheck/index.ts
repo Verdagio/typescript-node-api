6930cba830f87f1e194e0acf6d0829191a971385
Verdagio <dverdagio@gmail.com>
2019-02-03 21:04:13 +0000

    Refined Dockerfiles for production / debug config

--- src/routes/healthcheck/index.ts
+++ src/routes/healthcheck/index.ts
@@ -1,6 +1,6 @@
 // This route will merely verify that the api is reachable & that the router is functioning
 
 export const healthcheck = (data: object, callback: (statusCode: number, payload?: object) => object): void => {
-    callback(200, {'name': 'healthcheck'});
+    callback(200);
 }
 
